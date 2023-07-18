492230e35ed6a5b72a7dd340b067ed0bda9851f9
Indicattis <joao.staehler@gmail.com>
2023-07-18 12:15:30 -0300

    Update cliente-name-only.js

--- api/get/cliente-name-only.js
+++ api/get/cliente-name-only.js
@@ -9,15 +9,15 @@ app.use(cors());
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    const id = req.body;
+    const id_cliente = req.query.id_cliente;
   
     const checkQuery = `SELECT name FROM clientes WHERE id = ?`;
-    db.query(checkQuery, [id], (error, results) => {
+    db.query(checkQuery, [id_cliente], (error, results) => {
       if (error) {
         res.status(500).json({ error: 'Erro ao verificar número de telefone.' });
       } else {
         if (results.length > 0) {
-          const name = results[0];
+          const name = results[0].name;
           res.status(200).json({ name });
         } else {
           res.status(200).json({ name: '-' });
