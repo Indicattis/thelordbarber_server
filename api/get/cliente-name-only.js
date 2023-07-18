be572398150bc3c77655089d713d98af134022b6
Indicattis <joao.staehler@gmail.com>
2023-07-18 15:41:17 -0300

    Update cliente-name-only.js

--- api/get/cliente-name-only.js
+++ api/get/cliente-name-only.js
@@ -1,29 +1,26 @@
+// clientes.js
 const express = require ('express');
 const db = require ('../../db.js');
-const cors = require ('cors');
-
-const app = express();
-
-app.use(cors());
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-    const { id } = req.body;
-  
-    const checkQuery = `SELECT name FROM clientes WHERE id = ?`;
-    db.query(checkQuery, [id], (error, results) => {
-      if (error) {
-        res.status(500).json({ error: 'Erro ao verificar número de telefone.' });
-      } else {
-        if (results.length > 0) {
-          const name = results[0].name;
-          res.status(200).json({ name });
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT name FROM clientes WHERE id = ?`;
+    
+    db.query(sql, [id], (error, results) => {
+        if (error) {
+          res.status(500).json({ error: 'Erro ao verificar nome do cliente.' });
         } else {
-          res.status(200).json({ name: '-' });
+          if (results.length > 0) {
+            const clientName = results[0].name;
+            res.status(200).json({ name: clientName });
+          } else {
+            res.status(200).json({ name: '' });
+          }
         }
-      }
-    });
-  });
+      });
+});
+
 
 module.exports = router;
