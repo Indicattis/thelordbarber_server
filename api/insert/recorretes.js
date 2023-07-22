e6e3600f2452594eeb5c8839000f55ecff632bd9
Indicattis <joao.staehler@gmail.com>
2023-07-21 21:14:14 -0300

    Update recorretes.js

--- api/insert/recorretes.js
+++ api/insert/recorretes.js
@@ -7,52 +7,84 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-function calcularProximaData(recurrence_day, recurrence_hour, recurrence_mode) {
+// function calcularProximaData(recurrence_day, recurrence_hour, recurrence_mode) {
+//     const today = new Date();
+//     const nextMonth = new Date(today);
+//     nextMonth.setMonth(today.getMonth() + 1);
+//     const nextMonthYear = nextMonth.getFullYear();
+//     const nextMonthMonth = nextMonth.getMonth();
+
+//     const daysOfWeek = [null, 'dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']; // Correção nos índices
+
+//     const recurrenceDayIndex = daysOfWeek.indexOf(recurrence_day);
+
+//     const nextDates = [];
+//     const startTime = new Date(`1970-01-01T${recurrence_hour}`);
+
+//     if (recurrence_mode === 'semanal') {
+//         // Percorre todos os dias do mês seguinte e adiciona o dia de recorrência
+//         for (let day = 1; day <= new Date(nextMonthYear, nextMonthMonth + 1, 0).getDate(); day++) {
+//             const nextDate = new Date(nextMonthYear, nextMonthMonth, day, startTime.getHours(), startTime.getMinutes(), 0);
+//             if (nextDate.getDay() === recurrenceDayIndex) {
+//                 nextDates.push(nextDate);
+//             }
+//         }
+
+//     } else if (recurrence_mode === 'quinzenal') {
+//         // Encontra a primeira data agendada
+//         const firstDayOfMonth = new Date(nextMonthYear, nextMonthMonth, 1);
+//         let firstDay = firstDayOfMonth.getDate();
+//         const firstWeekday = firstDayOfMonth.getDay();
+//         if (firstWeekday <= recurrenceDayIndex) {
+//             firstDay += recurrenceDayIndex - firstWeekday;
+//         } else {
+//             firstDay += 7 - (firstWeekday - recurrenceDayIndex);
+//         }
+//         const firstDate = new Date(nextMonthYear, nextMonthMonth, firstDay, startTime.getHours(), startTime.getMinutes(), 0);
+
+//         // Verifica se a primeira data é menor ou igual ao último dia do mês seguinte
+//         while (firstDate <= new Date(nextMonthYear, nextMonthMonth + 1, 0)) {
+//             nextDates.push(firstDate);
+//             firstDay += 14;
+//             firstDate.setDate(firstDay);
+//         }
+//     }
+
+//     return nextDates;
+// }
+
+
+function calcularProximaData(recurrence_day, recurrence_hour) {
     const today = new Date();
     const nextMonth = new Date(today);
     nextMonth.setMonth(today.getMonth() + 1);
     const nextMonthYear = nextMonth.getFullYear();
     const nextMonthMonth = nextMonth.getMonth();
 
-    const daysOfWeek = [null, 'dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']; // Correção nos índices
+    const daysOfWeek = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab'];
 
     const recurrenceDayIndex = daysOfWeek.indexOf(recurrence_day);
 
     const nextDates = [];
     const startTime = new Date(`1970-01-01T${recurrence_hour}`);
 
-    if (recurrence_mode === 'semanal') {
-        // Percorre todos os dias do mês seguinte e adiciona o dia de recorrência
-        for (let day = 1; day <= new Date(nextMonthYear, nextMonthMonth + 1, 0).getDate(); day++) {
-            const nextDate = new Date(nextMonthYear, nextMonthMonth, day, startTime.getHours(), startTime.getMinutes(), 0);
-            if (nextDate.getDay() === recurrenceDayIndex) {
-                nextDates.push(nextDate);
-            }
-        }
-    } else if (recurrence_mode === 'quinzenal') {
-        // Encontra a primeira data agendada
-        const firstDayOfMonth = new Date(nextMonthYear, nextMonthMonth, 1);
-        let firstDay = firstDayOfMonth.getDate();
-        const firstWeekday = firstDayOfMonth.getDay();
-        if (firstWeekday <= recurrenceDayIndex) {
-            firstDay += recurrenceDayIndex - firstWeekday;
-        } else {
-            firstDay += 7 - (firstWeekday - recurrenceDayIndex);
-        }
-        const firstDate = new Date(nextMonthYear, nextMonthMonth, firstDay, startTime.getHours(), startTime.getMinutes(), 0);
+    // Encontra a primeira data de agendamento
+    let firstDay = today.getDate() + (recurrenceDayIndex - today.getDay());
+    if (firstDay <= today.getDate()) {
+        firstDay += 7;
+    }
 
-        // Verifica se a primeira data é menor ou igual ao último dia do mês seguinte
-        while (firstDate <= new Date(nextMonthYear, nextMonthMonth + 1, 0)) {
-            nextDates.push(firstDate);
-            firstDay += 14;
-            firstDate.setDate(firstDay);
-        }
+    const firstDate = new Date(today.getFullYear(), today.getMonth(), firstDay - 1, startTime.getHours(), startTime.getMinutes(), 0); // Subtraímos 1 dia para agendar um dia anterior
+
+    // Verifica se a primeira data é menor ou igual ao último dia do mês seguinte
+    while (firstDate <= new Date(nextMonthYear, nextMonthMonth + 1, 0)) {
+        nextDates.push(firstDate);
+        firstDay += 7;
+        firstDate.setDate(firstDay - 1); // Subtraímos 1 dia para agendar um dia anterior
     }
 
     return nextDates;
 }
-
-
 
 
 
@@ -79,7 +111,7 @@ router.post('/', (req, res) => {
                 recurrence_mode
             } = client;
 
-            const nextDates = calcularProximaData(recurrence_day, recurrence_hour, recurrence_mode);
+            const nextDates = calcularProximaData(recurrence_day, recurrence_hour);
 
             nextDates.forEach((date) => {
                 const formattedDate = date.toISOString().split('T')[0];
