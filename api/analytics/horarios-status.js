229bf96d5e205dd5126fd50a161d0ff0bdac3c4d
Indicattis <joao.staehler@gmail.com>
2023-07-25 12:10:25 -0300

    Update horarios-status.js

--- api/analytics/horarios-status.js
+++ api/analytics/horarios-status.js
@@ -9,15 +9,17 @@ app.use(cors());
 
 const router = express.Router();
 
+// ...
+
 router.get('/', (req, res) => {
-    const { id } = req.query;
+    const { id, month } = req.query; // Obtem o valor do mês enviado pelo Axios
     const currentDate = new Date();
-    const currentMonth = currentDate.getMonth() + 1; // Mês atual começa em 0, por isso somamos 1
+    const currentMonth = parseInt(month) || currentDate.getMonth() + 1; // Usa o valor enviado ou o mês atual
     const currentYear = currentDate.getFullYear();
-  
+    
     const sqlQuery = `SELECT COUNT(*) AS quantidadeLinhas FROM horarios WHERE id_barbeiro = ? AND MONTH(day) = ? AND YEAR(day) = ?`;
     const values = [id, currentMonth, currentYear];
-  
+    
     db.query(sqlQuery, values, (err, results) => {
       if (err) {
         console.error('Erro na consulta:', err);
@@ -28,6 +30,9 @@ router.get('/', (req, res) => {
       }
     });
   });
+  
+  // ...
+  
 
 
 module.exports = router;
