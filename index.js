af21f82c8be2231ecebe6e6628f2c206c61d2347
Indicattis <joao.staehler@gmail.com>
2023-08-03 20:20:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const port = process.env.PORT || 9001
+// const port = process.env.PORT || 9001
 
 // const getQRCode = require ('./bot/wppconnect.js')
 // app.use('/qrcode', getQRCode)
