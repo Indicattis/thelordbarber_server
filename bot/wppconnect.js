18527e25558fffbc266151b4e24fccebed627be4
Indicattis <joao.staehler@gmail.com>
2023-08-11 13:08:17 -0300

    Update wppconnect.js

--- bot/wppconnect.js
+++ bot/wppconnect.js
@@ -1,74 +1,65 @@
 
-const express = require('express');
-const fs = require('fs');
-const app = express();
 const wppconnect = require('@wppconnect-team/wppconnect');
+const express = require ('express');
 const cors = require ('cors');
 
-const SESSION_FILE_PATH = 'out.png';
 
+const app = express();
 app.use(express.json());
 app.use(cors());
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  if (fs.existsSync(SESSION_FILE_PATH)) {
-    return res.status(200).json({ qrCodePath: SESSION_FILE_PATH });
-  }
+router.get('/', (req, res) => {
+    wppconnect
+      .create({
+        session: 'sessionName',
+        catchQR: (base64Qr, asciiQR) => {
+          console.log(asciiQR); // Optional to log the QR in the terminal
+          var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+            response = {};
+  
+          if (matches.length !== 3) {
+            return new Error('Invalid input string');
+          }
+  
+          response.type = matches[1];
+          response.data = matches[2];
+  
+          // Send the QR code as a response to the frontend
+          // Note: You should handle the response type and status appropriately based on your API setup.
+          res.sendFile(__dirname + '/out.png');
+        },
+        logQR: false,
+      })
+    .then((client) => start(client))
+    .catch((error) => console.log(error));
 
-  try {
-    const client = await wppconnect.create({
-      session: 'sessionName',
-      catchQR: (base64Qr, asciiQR) => {
-        console.log(asciiQR);
-        var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-
-        if (matches.length !== 3) {
-          return res.status(500).json({ error: 'Invalid input string' });
+    function start(client) {
+      client.onMessage((message) => {
+        if (message.body === 'Olá') {
+          client
+            .sendText(message.from, 'Olá! Bem-vindo ao assistente virtual The Lord! Como posso lhe ajudar? \n\n 1 - Agendamento 🗓️ \n 2 - Cortes e Serviços ✂️ \n 3 - Suporte ao Usuário 👤')
+            .then((result) => {
+              console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+              console.error('Error when sending: ', erro); //return object error
+            });
         }
-
-        fs.writeFileSync(SESSION_FILE_PATH, matches[2], 'base64');
-      },
-      logQR: false,
-    });
-
-    start(client);
-
-    if (fs.existsSync(SESSION_FILE_PATH)) {
-      return res.status(200).json({ qrCodePath: SESSION_FILE_PATH });
-    } else {
-      return res.status(500).json({ error: 'No QR code data available' });
-    }
-  } catch (error) {
-    res.status(500).json({ error: 'Error initializing bot' });
-  }
-});
-
-
-function start(client) {
-  client.onMessage((message) => {
-    if (message.body === 'Olá') {
-      client
-        .sendText(message.from, 'Olá! Bem-vindo ao assistente virtual The Lord! Como posso lhe ajudar? \n\n 1 - Agendamento 🗓️ \n 2 - Cortes e Serviços ✂️ \n 3 - Suporte ao Usuário 👤')
-        .then((result) => {
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro); //return object error
-        });
-    }
-    if (message.body === '1') {
-        client
-          .sendText(message.from, 'Você pode realizar seu agendamento de qualquer lugar acessando: \n\n https://the-lord-barber-git-master-indicattis.vercel.app/agendamento \n\n Basta acessar o link e verificar os horários disponíveis! \n The Lord Barber agradece seu contato!')
-          .then((result) => {
-            console.log('Result: ', result); //return object success
-          })
-          .catch((erro) => {
-            console.error('Error when sending: ', erro); //return object error
-          });
-      }
-  });
+        if (message.body === '1') {
+            client
+              .sendText(message.from, 'Você pode realizar seu agendamento de qualquer lugar acessando: \n\n https://the-lord-barber-git-master-indicattis.vercel.app/agendamento \n\n Basta acessar o link e verificar os horários disponíveis! \n The Lord Barber agradece seu contato!')
+              .then((result) => {
+                console.log('Result: ', result); //return object success
+              })
+              .catch((erro) => {
+                console.error('Error when sending: ', erro); //return object error
+              });
+          }
+      });
 }
+}
+)
 
 module.exports = router
