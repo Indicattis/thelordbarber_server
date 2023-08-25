894726f8f68be516f68b444cf64c27574f385deb
Indicattis <joao.staehler@gmail.com>
2023-08-25 12:18:55 -0300

    SO MANY CHANGES

--- api/delete/barbeiro.js
+++ api/delete/barbeiro.js
@@ -1,6 +1,6 @@
-const express = require ('express');
-const db = require ('../../db.js');
-const cors = require ('cors');
+const express = require('express');
+const db = require('../../db.js');
+const cors = require('cors');
 
 const app = express();
 
@@ -8,47 +8,38 @@ app.use(cors());
 
 const router = express.Router();
 
-router.delete('/:id', (req, res) => {
+router.post('/:id', (req, res) => {
   const id = req.params.id;
 
   // Consulta para excluir os horários relacionados ao barbeiro
   const deleteHorariosQuery = 'DELETE FROM horarios WHERE id_barbeiro = ?';
 
+  // Consulta para excluir os agendamentos relacionados ao barbeiro
+  const deleteAgendamentosQuery = 'DELETE FROM agendamentos WHERE id_barbeiro = ?';
+
   // Consulta para excluir o barbeiro
   const deleteBarbeiroQuery = 'DELETE FROM barbeiros WHERE id = ?';
 
-  db.beginTransaction((beginTransactionError) => {
-    if (beginTransactionError) {
-      res.status(500).json({ error: 'Erro ao iniciar a transação.' });
-      return;
-    }
-
-    db.query(deleteHorariosQuery, [id], (deleteHorariosError) => {
-      if (deleteHorariosError) {
-        db.rollback(() => {
-          res.status(500).json({ error: 'Erro ao excluir os horários do barbeiro.' });
-        });
-      } else {
-        db.query(deleteBarbeiroQuery, [id], (deleteBarbeiroError) => {
-          if (deleteBarbeiroError) {
-            db.rollback(() => {
+  db.query(deleteHorariosQuery, [id], (deleteHorariosError) => {
+    if (deleteHorariosError) {
+      res.status(500).json({ error: 'Erro ao excluir os horários do barbeiro.' });
+    } else {
+      db.query(deleteAgendamentosQuery, [id], (deleteAgendamentoError) => {
+        if (deleteAgendamentoError) {
+          res.status(500).json({ error: 'Erro ao excluir os agendamentos do barbeiro.' });
+        } else {
+          db.query(deleteBarbeiroQuery, [id], (deleteBarbeiroError) => {
+            if (deleteBarbeiroError) {
               res.status(500).json({ error: 'Erro ao deletar barbeiro.' });
-            });
-          } else {
-            db.commit((commitError) => {
-              if (commitError) {
-                db.rollback(() => {
-                  res.status(500).json({ error: 'Erro ao confirmar a transação.' });
-                });
-              } else {
-                res.status(200).json({ message: 'Barbeiro deletado com sucesso.' });
-              }
-            });
-          }
-        });
-      }
-    });
+            } else {
+              res.status(200).json({ message: 'Barbeiro deletado com sucesso.' });
+            }
+          });
+        }
+      });
+    }
   });
 });
 
 module.exports = router;
+
